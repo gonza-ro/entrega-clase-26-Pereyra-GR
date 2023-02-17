9ddd8c8b74bb3cb433f574823e5c8f063870dd4e
gonza-ro <gonzalorp12345@gmail.com>
2023-02-17 00:33:08 -0300

    al fin mundo cruel

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const faker = require('@faker-js/faker')
+const socket = require('socket.io');
 
+
+
+
+const app = express()
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+/* Configuración del servidor */
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log('Tu servidor esta funcionando en el puerto: ', PORT);
+})
+server.on('error', error => console.log('Error en el servidor', error));
+const io =  socket(server)
+
+io.on('connection', socket => {
+    console.log('Nuevo cliente conectado');
+
+    socket.emit('mensajes', mensajes)
+
+    socket.on('mensaje', data => {
+        mensajes.push({socketid: socket.id, mensaje: data})
+        io.sockets.emit('mensajes', mensajes)
+    });
+});
